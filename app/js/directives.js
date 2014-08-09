1f119c10f284b902c46aba27404822111ca7b90d
Matteo Borgato <borgato.matteo@gmail.com>
2014-08-09 19:01:32 +0100

    added some stylesheet

--- app/js/directives.js
+++ app/js/directives.js
@@ -21,8 +21,8 @@ dir.directive('wallet', [
   '$rootScope',
   function( $rootScope ) {
 
-    var ADD    = 'add';
-    var REMOVE = 'remove';
+    var ADD    = '+';
+    var REMOVE = '-';
 
     return {
       restrict: 'E',
@@ -30,15 +30,38 @@ dir.directive('wallet', [
       templateUrl: 'partials/wallet.html',
       link: function( scope, element, attribute ) {
 
-        scope.transitions = [];
-        scope.total       = 0;
+        scope.currencies = [
+          {
+            "active": true,
+            "val": "Euro",
+            "symbol": "€"
+          }, {
+            "active": false,
+            "val": "GBP",
+            "symbol": "£"
+          }
+        ];
+        scope.activeCurrency = scope.currencies[0];
+        scope.transitions    = [];
+        scope.total          = 0;
 
+        scope.setCurrency = function( index ) {
+          // already selected
+          if ( scope.currencies[ index ].active ) {
+            return;
+          }
 
+          for ( var i = 0, nodes = scope.currencies.length; i < nodes; i++ ) {
+            scope.currencies[ i ].active = false;
+          }
+          scope.currencies[ index ].active = true;
+          scope.activeCurrency = scope.currencies[index];
+        };
 
         scope.addAmount = function() {
           var amount = 10;
-          scope.total += amount;
-          
+          scope.total += scope.amout;
+
           scope.transitions.push({
             "type": ADD,
             "amount": amount,
